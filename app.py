69ef80f643ca97d84050e3f56e6d1a7e9c179f88
nitesh-kumbalkar <nitesh.kumbalkar@gmail.com>
2020-03-22 22:08:14 +0530

    Created app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, abort, jsonify
+from model.request import RequestSchema
+from src.service import process_request
+app = Flask(__name__)
+
+req_schema = RequestSchema()
+
+@app.route('/api', methods=['POST'])
+def github_api():
+    # Payload validation
+    payload = request.get_json()
+    errors = req_schema.validate(payload)
+    if errors:
+        return jsonify(errors), 400
+    else:
+        return jsonify(process_request(payload)), 200
+
+if __name__ == '__main__':
+    app.run()
